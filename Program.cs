2b24a37be8d0f94ceb9f65bb38f5acba045dc994
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-17 16:06:36 -0300

    Projeto Tabuada finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Projeto_Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i =0;
+            int num;
+            int resultado;
+            Console.WriteLine("Digite um número inteiro para saber sua tabuada.");
+            num = int.Parse(Console.ReadLine());
+
+            for(i=0; i<=10; i++){
+                resultado = num*i;
+                Console.WriteLine(+num+ "x"+i+" = "+resultado);
+            }
+
+        }
+    }
+}
